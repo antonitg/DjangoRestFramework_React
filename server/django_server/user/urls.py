9d0ab15a27c5a64b588941b99f7b28407ec021b4
Antoni Tormo Garcia <gineraantoni@gmail.com>
2022-01-12 13:15:02 +0100

    Created register endpoint and view and migrated User serializer to Register serializer

--- server/django_server/user/urls.py
+++ server/django_server/user/urls.py
@@ -5,9 +5,9 @@ from rest_framework_simplejwt import views as jwt_views
 
 app_name = 'user'
 urlpatterns = [
-    path('v1/user', User_APIView.as_view()), 
-    # path('v1/post/<int:pk>/', User_APIView_Detail.as_view()),
-    path('v1/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('v1/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/info', Info_APIView.as_view()), 
+    path('v1/register/', Register_APIView.as_view(), name='auth_register'),
+    path('v1/login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/login/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
--- server/django_server/user/views.py
+++ server/django_server/user/views.py
@@ -4,13 +4,19 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserSerializers
-from .models import User
-from rest_framework import status
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework import status, generics
 from django.http import Http404
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-class User_APIView(APIView):
+class Register_APIView(generics.CreateAPIView):
+    # def post(self request)
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+class Info_APIView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request, format=None, *args, **kwargs):
         user = User.objects.all()
