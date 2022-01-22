f8b00b279266cdb00b83878b49ab860b58d1169e
Antoni Tormo Garcia <gineraantoni@gmail.com>
2022-01-22 13:01:16 +0100

    Migrated the query hooks softdeleted

--- client/src/hooks/getStations.js
+++ client/src/hooks/getStations.js
@@ -1,6 +1,10 @@
-import { useQuery } from 'react-query'
-import apiClient from '../core/http-common'
+// import { useQuery } from 'react-query'
+// import apiClient from '../core/http-common'
 
-export function GetStations() {
-    return useQuery(['stations', 'getStations'], async () => await apiClient.get("/v1/stations/"))
-}
+// export function GetStations() {
+//     return useQuery(['stations', 'getStations'], async () => await apiClient.get("/v2/stations/"))
+// }
+
+            
+// Not needed anymore, retrieve data directly on functions or hooks and apply the result on the setQueryData().
+// Example-> apiClient.get("/v2/stations/").then(result => queryClient.setQueryData('stations', () => (result.data)))
