f8aebe95d9d758af7c8dd0f53261652fdbbb0fd5
Antoni Tormo Garcia <gineraantoni@gmail.com>
2022-01-27 20:50:44 +0100

    Cleaning code

--- client/src/routes/objectives.js
+++
@@ -1,7 +0,0 @@
-export default function Objectives() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Objectives</h2>
-      </main>
-    );
-  }
--- client/src/routes/rest.js
+++
@@ -1,7 +0,0 @@
-export default function Rest() {
-    return (
-      <div style={{ padding: "1rem 0", }} className="col">
-        <h2>Rest</h2>
-      </div>
-    );
-  }
